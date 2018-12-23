68114662b85bfd1a6d397f8605d9df90e519ff97
shrbenic1 <shrbenic1@etf.unsa.ba>
2018-12-23 19:03:28 +0100

    Implementirana metoda ispisiGradove

--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -5,6 +5,15 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static String ispisiGradove() {
+        ArrayList<Grad> gradovi = GeografijaDAO.getInstance().gradovi();
+        String spisak = "";
+        for(Grad grad: gradovi) {
+            spisak+= grad.getNaziv() + " (" + grad.getDrzava().getNaziv() + ")" + " - " + grad.getBrojStanovnika() + "\n";
+        }
+        return spisak;
+    }
+
     public static void main(String[] args) {
         System.out.println("Gradovi su:\n" + ispisiGradove());
         glavniGrad();
