4c768c812e6d7790e73c56fe3c7a9169fda2739b
shrbenic1 <shrbenic1@etf.unsa.ba>
2018-12-23 13:56:44 +0100

    Kreirani getteri

--- src/ba/unsa/etf/rpr/Grad.java
+++ src/ba/unsa/etf/rpr/Grad.java
@@ -10,6 +10,22 @@ public class Grad {
 
     }
 
+    public Integer getId() {
+        return id;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public Integer getBrojStanovnika() {
+        return brojStanovnika;
+    }
+
+    public Drzava getDrzava() {
+        return drzava;
+    }
+
     public Grad(int id, String naziv, int brojStanovnika, Drzava drzava) {
         this.id = id;
         this.naziv = naziv;
