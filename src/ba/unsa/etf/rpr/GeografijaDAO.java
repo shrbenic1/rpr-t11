8f7f9ad2a6408147c84128160f555b54281297f7
shrbenic1 <shrbenic1@etf.unsa.ba>
2018-12-23 15:28:31 +0100

    Podržano kreiranje prazne tabele ukoliko baza ne postoji

--- src/ba/unsa/etf/rpr/GeografijaDAO.java
+++ src/ba/unsa/etf/rpr/GeografijaDAO.java
@@ -1,25 +1,71 @@
 package ba.unsa.etf.rpr;
 
 import java.io.File;
-import java.sql.Connection;
+import java.sql.*;
 import java.util.ArrayList;
 
 public class GeografijaDAO {
     private static GeografijaDAO instance = null;
     private static Connection conn;
-    ArrayList<Grad> gradovi;
-    ArrayList<Drzava> drzave;
+    private ArrayList<Grad> gradovi = new ArrayList<>();
+    private ArrayList<Drzava> drzave = new ArrayList<>();
 
     private static void initialize() {
+        if (instance == null) {
+            createNewDatabase();
+            createNewTable();
+            instance.insert();
+        }
+        instance = new GeografijaDAO();
+    }
 
+    private  void insert() {
+
+    }
+
+    private static void createNewTable() {
+        String grad = "CREATE TABLE IF NOT EXISTS grad\n" +
+                "(\n" +
+                "    id int PRIMARY KEY,\n" +
+                "    naziv text,\n" +
+                "    broj_stanovnika int,\n" +
+                "    drzava int,\n" +
+                "    CONSTRAINT grad_drzava_id_fk FOREIGN KEY (drzava) REFERENCES drzava (id)\n" +
+                ");";
+        String drzava = "CREATE TABLE IF NOT EXISTS drzava\n" +
+                "(\n" +
+                "    id int PRIMARY KEY,\n" +
+                "    naziv text,\n" +
+                "    glavni_grad int,\n" +
+                "    CONSTRAINT drzava_grad_id_fk FOREIGN KEY (glavni_grad) REFERENCES grad (id)\n" +
+                ");";
+        try (PreparedStatement statement1 = conn.prepareStatement(grad);
+             PreparedStatement statement2 = conn.prepareStatement(drzava)) {
+            statement1.execute();
+            statement2.execute();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static void createNewDatabase() {
+        String url = "jdbc:sqlite:baza.db";
+        try (Connection conn = DriverManager.getConnection(url)) {
+            if (conn != null) {
+                DatabaseMetaData meta = conn.getMetaData();
+                System.out.println("A new database has been created.");
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     private GeografijaDAO() {
-        File file = new File("baza.db");
+
     }
 
     public static GeografijaDAO getInstance() {
-        if(instance == null) initialize();
+        if (instance == null) initialize();
         return instance;
     }
 
