7540a46fa360aba5abe180fccada449889ac4f18
shrbenic1 <shrbenic1@etf.unsa.ba>
2018-12-23 13:58:50 +0100

    Kreirani getteri i setteri

--- src/ba/unsa/etf/rpr/Drzava.java
+++ src/ba/unsa/etf/rpr/Drzava.java
@@ -14,4 +14,28 @@ public class Drzava {
         this.naziv = naziv;
         this.glavniGrad = glavniGrad;
     }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public Grad getGlavniGrad() {
+        return glavniGrad;
+    }
+
+    public void setGlavniGrad(Grad glavniGrad) {
+        this.glavniGrad = glavniGrad;
+    }
 }
