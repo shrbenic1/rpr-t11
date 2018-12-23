ec6f6302a5e4e030097282705d1cae033c81ebbd
shrbenic1 <shrbenic1@etf.unsa.ba>
2018-12-23 13:53:13 +0100

    Kreirane deklaracije metoda

--- src/ba/unsa/etf/rpr/GeografijaDAO.java
+++ src/ba/unsa/etf/rpr/GeografijaDAO.java
@@ -5,4 +5,33 @@ import java.sql.Connection;
 public class GeografijaDAO {
     private static GeografijaDAO instance = null;
     private static Connection conn;
+
+    Grad glavniGrad(String drzava) {
+        return null;
+    }
+
+    void obrisiDrzavu(String drzava) {
+
+    }
+
+    ArrayList<Grad> gradovi() {
+        return null;
+    }
+
+    void dodajGrad(Grad grad) {
+
+    }
+
+    void dodajDrzavu(Drzava drzava) {
+
+    }
+
+    void izmijeniGrad(Grad grad) {
+
+    }
+
+    Drzava nadjiDrzavu(String drzava) {
+        return null;
+    }
 }
+
