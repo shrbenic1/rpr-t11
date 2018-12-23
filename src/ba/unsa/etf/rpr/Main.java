f8e82038dd4ceb8fabfd2b9f53d00725cc10c1f2
shrbenic1 <shrbenic1@etf.unsa.ba>
2018-12-23 19:05:10 +0100

    Implementirana metoda glavniGrad

--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -14,6 +14,19 @@ public class Main {
         return spisak;
     }
 
+    public  static void glavniGrad() {
+        System.out.println("Unesite ime države: ");
+        Scanner ulaz = new Scanner(System.in);
+        String drzava = ulaz.nextLine();
+        Grad grad = GeografijaDAO.getInstance().glavniGrad(drzava);
+        if(grad!=null) {
+            System.out.println("Glavni grad države " + drzava + "je " + grad.getNaziv());
+        }
+        else {
+            System.out.println("Nepostojeća država");
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Gradovi su:\n" + ispisiGradove());
         glavniGrad();
