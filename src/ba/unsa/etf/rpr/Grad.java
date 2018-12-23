a0a025a0231115260652f9e60b0ee710221aaf08
shrbenic1 <shrbenic1@etf.unsa.ba>
2018-12-23 13:56:24 +0100

    Kreirani konstruktori

--- src/ba/unsa/etf/rpr/Grad.java
+++ src/ba/unsa/etf/rpr/Grad.java
@@ -6,5 +6,14 @@ public class Grad {
     private Integer brojStanovnika;
     private Drzava drzava;
 
+    public Grad() {
 
+    }
+
+    public Grad(int id, String naziv, int brojStanovnika, Drzava drzava) {
+        this.id = id;
+        this.naziv = naziv;
+        this.brojStanovnika = brojStanovnika;
+        this.drzava = drzava;
+    }
 }
